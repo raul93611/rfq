b579504c7a419dd5ff7108d48024e204ebad490d
raul_trabajo <raul93611@gmail.com>
2018-06-08 15:57:02 -0400

    se restringio la vista de graficos

--- plantillas/muro_default.inc.php
+++ plantillas/muro_default.inc.php
@@ -63,16 +63,16 @@ Conexion::cerrar_conexion();
                 <div class="col-lg-12 col-12">
                     <!-- small box -->
                     <input type="hidden" id="nombres_usuario" <?php echo "value='" . json_encode($nombres_usuario) . "'"; ?>>
-<input type="hidden" id="cotizaciones_completadas" <?php echo "value='" . json_encode($cotizaciones_completadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_completadas_pasadas" <?php echo "value='" . json_encode($cotizaciones_completadas_pasadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_ganadas" <?php echo "value='" . json_encode($cotizaciones_ganadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_ganadas_pasadas" <?php echo "value='" . json_encode($cotizaciones_ganadas_pasadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_sometidas" <?php echo "value='" . json_encode($cotizaciones_sometidas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_sometidas_pasadas" <?php echo "value='" . json_encode($cotizaciones_sometidas_pasadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_no_sometidas" <?php echo "value='" . json_encode($cotizaciones_no_sometidas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_no_sometidas_pasadas" <?php echo "value='" . json_encode($cotizaciones_no_sometidas_pasadas) . "'"; ?>>
-<input type="hidden" id="cotizaciones_mes" <?php echo "value='" . json_encode($cotizaciones_mes) . "'"; ?>>
-<input type="hidden" id="monto_cotizaciones_mes" <?php echo "value='" . json_encode($monto_cotizaciones_mes) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_completadas" <?php echo "value='" . json_encode($cotizaciones_completadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_completadas_pasadas" <?php echo "value='" . json_encode($cotizaciones_completadas_pasadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_ganadas" <?php echo "value='" . json_encode($cotizaciones_ganadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_ganadas_pasadas" <?php echo "value='" . json_encode($cotizaciones_ganadas_pasadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_sometidas" <?php echo "value='" . json_encode($cotizaciones_sometidas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_sometidas_pasadas" <?php echo "value='" . json_encode($cotizaciones_sometidas_pasadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_no_sometidas" <?php echo "value='" . json_encode($cotizaciones_no_sometidas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_no_sometidas_pasadas" <?php echo "value='" . json_encode($cotizaciones_no_sometidas_pasadas) . "'"; ?>>
+                    <input type="hidden" id="cotizaciones_mes" <?php echo "value='" . json_encode($cotizaciones_mes) . "'"; ?>>
+                    <input type="hidden" id="monto_cotizaciones_mes" <?php echo "value='" . json_encode($monto_cotizaciones_mes) . "'"; ?>>
                 </div>
                 <!-- ./col -->
                 <!--<div class="col-lg-3 col-6">
@@ -114,55 +114,62 @@ Conexion::cerrar_conexion();
                             </div>
                         </div>
                     </div>-->
-                    <div class="card">
-                        <div class="card-header no-border">
-                            <div class="d-flex justify-content-between">
-                                <h3 class="card-title">Completed</h3>
+                    <?php
+                    if ($cargo != 4) {
+                        ?>
+                        <div class="card">
+                            <div class="card-header no-border">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="card-title">Completed</h3>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <!-- /.d-flex -->
+
+                                <div class="position-relative mb-4">
+                                    <canvas id="completados-chart" height="200"></canvas>
+                                </div>
+
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="mr-2">
+                                        <i class="fa fa-square text-primary"></i> Current month
+                                    </span>
+
+                                    <span>
+                                        <i class="fa fa-square text-gray"></i> Last month
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body">
-                            <!-- /.d-flex -->
 
-                            <div class="position-relative mb-4">
-                                <canvas id="completados-chart" height="200"></canvas>
+                        <div class="card">
+                            <div class="card-header no-border">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="card-title">No submitted</h3>
+                                </div>
                             </div>
+                            <div class="card-body">
+                                <!-- /.d-flex -->
 
-                            <div class="d-flex flex-row justify-content-end">
-                                <span class="mr-2">
-                                    <i class="fa fa-square text-primary"></i> Current month
-                                </span>
+                                <div class="position-relative mb-4">
+                                    <canvas id="sometidas-chart" height="200"></canvas>
+                                </div>
 
-                                <span>
-                                    <i class="fa fa-square text-gray"></i> Last month
-                                </span>
-                            </div>
-                        </div>
-                    </div>
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="mr-2">
+                                        <i class="fa fa-square text-primary"></i> Current month
+                                    </span>
 
-                    <div class="card">
-                        <div class="card-header no-border">
-                            <div class="d-flex justify-content-between">
-                                <h3 class="card-title">No submitted</h3>
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <!-- /.d-flex -->
-
-                            <div class="position-relative mb-4">
-                                <canvas id="sometidas-chart" height="200"></canvas>
-                            </div>
-
-                            <div class="d-flex flex-row justify-content-end">
-                                <span class="mr-2">
-                                    <i class="fa fa-square text-primary"></i> Current month
-                                </span>
-
-                                <span>
-                                    <i class="fa fa-square text-gray"></i> Last month
-                                </span>
+                                    <span>
+                                        <i class="fa fa-square text-gray"></i> Last month
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                        <?php
+                    }
+                    ?>
+
                     <div class="card">
                         <div class="card-header no-border">
                             <div class="d-flex justify-content-between">
@@ -196,31 +203,38 @@ Conexion::cerrar_conexion();
                 <!-- /.Left col -->
                 <!-- right col (We are only adding the ID to make the widgets sortable)-->
                 <section class="col-lg-6 connectedSortable">
-                    <div class="card">
-                        <div class="card-header no-border">
-                            <div class="d-flex justify-content-between">
-                                <h3 class="card-title">Awards</h3>
+                    <?php
+                    if ($cargo != 4) {
+                        ?>
+                        <div class="card">
+                            <div class="card-header no-border">
+                                <div class="d-flex justify-content-between">
+                                    <h3 class="card-title">Awards</h3>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <!-- /.d-flex -->
+
+                                <div class="position-relative mb-4">
+                                    <canvas id="ganadas-chart" height="200"></canvas>
+                                </div>
+
+                                <div class="d-flex flex-row justify-content-end">
+                                    <span class="mr-2">
+                                        <i class="fa fa-square text-primary"></i> Current month
+                                    </span>
+
+                                    <span>
+                                        <i class="fa fa-square text-gray"></i> Last month
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                        <div class="card-body">
-                            <!-- /.d-flex -->
+                        <?php
+                    }
+                    ?>
 
-                            <div class="position-relative mb-4">
-                                <canvas id="ganadas-chart" height="200"></canvas>
-                            </div>
 
-                            <div class="d-flex flex-row justify-content-end">
-                                <span class="mr-2">
-                                    <i class="fa fa-square text-primary"></i> Current month
-                                </span>
-
-                                <span>
-                                    <i class="fa fa-square text-gray"></i> Last month
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                    
                     <div class="card">
                         <div class="card-header no-border">
                             <div class="d-flex justify-content-between">
