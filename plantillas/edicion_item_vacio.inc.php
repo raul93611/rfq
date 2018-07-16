81a7ecaa3e5e54690003678ba9533f378878ceb3
raul_trabajo <raul93611@gmail.com>
2018-07-16 10:38:07 -0400

    se quito required en edicion de item

--- plantillas/edicion_item_vacio.inc.php
+++ plantillas/edicion_item_vacio.inc.php
@@ -6,30 +6,30 @@
             <h2>Project specifications</h2>
             <div class="form-group">
                 <label for="brand_project">Brand:</label>
-                <input type="text" class="form-control" id="brand_project" name="brand_project" placeholder="Brand ..." autofocus required value="<?php echo $item->obtener_brand_project(); ?>">
+                <input type="text" class="form-control" id="brand_project" name="brand_project" placeholder="Brand ..." autofocus value="<?php echo $item->obtener_brand_project(); ?>">
             </div>
             <div class="form-group">
                 <label for="part_number_project">Part #:</label>
-                <input type="text" class="form-control" id="part_number_project" name="part_number_project" placeholder="Part # ..." required value="<?php echo $item->obtener_part_number(); ?>">
+                <input type="text" class="form-control" id="part_number_project" name="part_number_project" placeholder="Part # ..." value="<?php echo $item->obtener_part_number(); ?>">
             </div>
             <div class="form-group">
                 <label for="description_project">Description:</label>
-                <textarea class="form-control" rows="5" placeholder="Enter description ..." id="description_project" name="description_project" required><?php echo $item->obtener_description_project(); ?></textarea>
+                <textarea class="form-control" rows="5" placeholder="Enter description ..." id="description_project" name="description_project"><?php echo $item->obtener_description_project(); ?></textarea>
             </div>
         </div>
         <div class="col">
             <h2>E-logic proposal</h2>
             <div class="form-group">
                 <label for="brand">Brand:</label>
-                <input type="text" class="form-control" id="brand" name="brand" placeholder="Brand ..." autofocus required value="<?php echo $item->obtener_brand(); ?>">
+                <input type="text" class="form-control" id="brand" name="brand" placeholder="Brand ..." autofocus value="<?php echo $item->obtener_brand(); ?>">
             </div>
             <div class="form-group">
                 <label for="part_number">Part #:</label>
-                <input type="text" class="form-control" id="part_number" name="part_number" placeholder="Part # ..." required value="<?php echo $item->obtener_part_number(); ?>">
+                <input type="text" class="form-control" id="part_number" name="part_number" placeholder="Part # ..." value="<?php echo $item->obtener_part_number(); ?>">
             </div>
             <div class="form-group">
                 <label for="description">Description:</label>
-                <textarea class="form-control" rows="5" placeholder="Enter description ..." id="description" name="description" required><?php echo $item->obtener_description(); ?></textarea>
+                <textarea class="form-control" rows="5" placeholder="Enter description ..." id="description" name="description"><?php echo $item->obtener_description(); ?></textarea>
             </div>
         </div>
     </div>
