db316999cd633199414af4df107f0c3dfd16da79
raul-trabajo <raul93611@gmail.com>
2018-07-23 17:05:53 -0400

    cambio de version

--- plantillas/documento_cierre.inc.php
+++ plantillas/documento_cierre.inc.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <strong>Copyright &copy; 2018.</strong>
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 0.1
+        <b>Version</b> 1.0
     </div>
 </footer>
 </div>
