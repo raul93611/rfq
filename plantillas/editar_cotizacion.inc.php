aede256fb69be761085ab0c4908fac20165fae0a
Leonardo Velasco <raul93611@gmail.com>
2018-11-21 09:36:58 -0400

    change column size

--- plantillas/editar_cotizacion.inc.php
+++ plantillas/editar_cotizacion.inc.php
@@ -13,10 +13,10 @@ if(is_null($cotizacion_recuperada)){
   <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-4">
+        <div class="col-sm-2">
           <h1>Internal quote</h1>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-8 text-center">
           <?php
           if(!$cotizacion_recuperada-> obtener_rfp()){
             ?>
