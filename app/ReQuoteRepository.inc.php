b071863c36a22c5cc275cd74a3acee853422a2be
Raul Velasco <raul93611@gmail.com>
2019-02-11 19:44:43 -0400

    take FedBid quotes like regular quotes

--- app/ReQuoteRepository.inc.php
+++ app/ReQuoteRepository.inc.php
@@ -109,204 +109,183 @@ class ReQuoteRepository{
     $re_quote = self::get_re_quote_by_id_rfq(Conexion::obtener_conexion(), $id_rfq);
     $re_quote_items = ReQuoteItemRepository::get_re_quote_items_by_id_re_quote(Conexion::obtener_conexion(), $re_quote-> get_id());
     Conexion::cerrar_conexion();
-    if($cotizacion-> obtener_canal() == 'FedBid'){
+    ?>
+    <div class="row">
+      <div class="col-md-3">
+        <b>Taxes:</b> <?php echo $re_quote-> get_taxes(); ?>%<br>
+      </div>
+      <div class="col-md-3">
+        <b>Profit:</b> <?php echo $re_quote-> get_profit(); ?>%<br>
+      </div>
+      <div class="col-md-3">
+        <b>Additional general:</b> $ <?php echo $re_quote-> get_additional(); ?>
+      </div>
+      <div class="col-md-3">
+        <b>Payment terms:</b> <?php echo $re_quote-> get_payment_terms(); ?>
+      </div>
+    </div>
+    <br>
+    <?php
+    if (count($re_quote_items)) {
       ?>
-      <div class="row">
-        <div class="col-md-6">
-          <label>Total cost:</label>
-        </div>
-        <div class="col-md-6">
-          $ <?php echo $re_quote-> get_total_cost(); ?>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-6">
-          <label>Total price:</label>
-        </div>
-        <div class="col-md-6">
-          $ <?php echo $re_quote-> get_total_price(); ?>
-        </div>
-      </div>
+      <table class="table table-bordered" style="width:100%;">
+        <tr>
+          <th class="quantity">#</th>
+          <th>PROJECT ESPC.</th>
+          <th>E-LOGIC PROP.</th>
+          <th class="quantity">QTY</th>
+          <th>PROVIDER</th>
+          <th>ADDITIONAL</th>
+          <th>BEST UNIT COST</th>
+          <th>TOTAL COST</th>
+          <th>PRICE FOR CLIENT</th>
+          <th>TOTAL PRICE</th>
+        </tr>
       <?php
-    }else{
-      ?>
-      <div class="row">
-        <div class="col-md-3">
-          <b>Taxes:</b> <?php echo $re_quote-> get_taxes(); ?>%<br>
-        </div>
-        <div class="col-md-3">
-          <b>Profit:</b> <?php echo $re_quote-> get_profit(); ?>%<br>
-        </div>
-        <div class="col-md-3">
-          <b>Additional general:</b> $ <?php echo $re_quote-> get_additional(); ?>
-        </div>
-        <div class="col-md-3">
-          <b>Payment terms:</b> <?php echo $re_quote-> get_payment_terms(); ?>
-        </div>
-      </div>
-      <br>
-      <?php
-      if (count($re_quote_items)) {
+      $a = 1;
+      if($re_quote-> get_payment_terms() == 'Net 30/CC'){
+        $payment_terms = 1.0215;
+      }else{
+        $payment_terms = 1;
+      }
+      for ($i = 0; $i < count($re_quote_items); $i++) {
+        $re_quote_item = $re_quote_items[$i];
         ?>
-        <table class="table table-bordered" style="width:100%;">
-          <tr>
-            <th class="quantity">#</th>
-            <th>PROJECT ESPC.</th>
-            <th>E-LOGIC PROP.</th>
-            <th class="quantity">QTY</th>
-            <th>PROVIDER</th>
-            <th>ADDITIONAL</th>
-            <th>BEST UNIT COST</th>
-            <th>TOTAL COST</th>
-            <th>PRICE FOR CLIENT</th>
-            <th>TOTAL PRICE</th>
-          </tr>
+        <tr>
+          <td><?php echo $a; ?></td>
+          <td><b>Brand name:</b><?php echo $re_quote_item-> get_brand_project(); ?><br><b>Part number:</b><?php echo $re_quote_item-> get_part_number_project(); ?><br><b>Item description:</b><?php echo nl2br(mb_substr($re_quote_item-> get_description_project(), 0, 150)); ?></td>
+          <td><b>Brand name:</b><?php echo $re_quote_item-> get_brand(); ?><br><b>Part number:</b><?php echo $re_quote_item-> get_part_number(); ?><br><b> Item description:</b><br><?php echo nl2br(mb_substr($re_quote_item-> get_description(), 0, 150)); ?></td>
+          <td style="text-align:right;"><?php echo $re_quote_item-> get_quantity(); ?></td>
+          <td style="width: 200px;">
         <?php
-        $a = 1;
-        if($re_quote-> get_payment_terms() == 'Net 30/CC'){
-          $payment_terms = 1.0215;
-        }else{
-          $payment_terms = 1;
-        }
-        for ($i = 0; $i < count($re_quote_items); $i++) {
-          $re_quote_item = $re_quote_items[$i];
-          ?>
-          <tr>
-            <td><?php echo $a; ?></td>
-            <td><b>Brand name:</b><?php echo $re_quote_item-> get_brand_project(); ?><br><b>Part number:</b><?php echo $re_quote_item-> get_part_number_project(); ?><br><b>Item description:</b><?php echo nl2br(mb_substr($re_quote_item-> get_description_project(), 0, 150)); ?></td>
-            <td><b>Brand name:</b><?php echo $re_quote_item-> get_brand(); ?><br><b>Part number:</b><?php echo $re_quote_item-> get_part_number(); ?><br><b> Item description:</b><br><?php echo nl2br(mb_substr($re_quote_item-> get_description(), 0, 150)); ?></td>
-            <td style="text-align:right;"><?php echo $re_quote_item-> get_quantity(); ?></td>
-            <td style="width: 200px;">
-          <?php
-          Conexion::abrir_conexion();
-          $re_quote_providers = ReQuoteProviderRepository::get_re_quote_providers_by_id_re_quote_item(Conexion::obtener_conexion(), $re_quote_item-> get_id());
-          Conexion::cerrar_conexion();
-          $prices = [];
-          if(count($re_quote_providers)){
-            foreach ($re_quote_providers as $re_quote_provider) {
-              $prices[] = $re_quote_provider-> get_price();
-              ?>
-              <div class="row">
-                <div class="col-md-6">
-                  <b><?php echo $re_quote_provider-> get_provider(); ?>:</b>
-                </div>
-                <div class="col-md-6">
-                  $ <?php echo number_format($re_quote_provider-> get_price(), 2); ?>
-                </div>
+        Conexion::abrir_conexion();
+        $re_quote_providers = ReQuoteProviderRepository::get_re_quote_providers_by_id_re_quote_item(Conexion::obtener_conexion(), $re_quote_item-> get_id());
+        Conexion::cerrar_conexion();
+        $prices = [];
+        if(count($re_quote_providers)){
+          foreach ($re_quote_providers as $re_quote_provider) {
+            $prices[] = $re_quote_provider-> get_price();
+            ?>
+            <div class="row">
+              <div class="col-md-6">
+                <b><?php echo $re_quote_provider-> get_provider(); ?>:</b>
               </div>
-              <?php
-            }
-            ?>
-            </td>
-            <td>$ <?php echo number_format($re_quote_item-> get_additional(), 2); ?></td>
-            <td>$
-            <?php
-            $best_unit_price = min($prices)*$payment_terms*(1+($re_quote-> get_taxes()/100)) + $re_quote_item-> get_additional() + $re_quote-> get_additional();
-            echo number_format($best_unit_price, 2);
-            ?>
-            </td>
-            <td>$ <?php echo number_format(round($best_unit_price, 2) * $re_quote_item-> get_quantity(), 2); ?></td>
-            <td style="text-align:right;">$ <?php echo number_format($re_quote_item-> get_unit_price(), 2); ?></td>
-            <td style="text-align:right;">$ <?php echo number_format($re_quote_item-> get_total_price(), 2); ?></td>
-            <?php
-          }else{
-            ?>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+              <div class="col-md-6">
+                $ <?php echo number_format($re_quote_provider-> get_price(), 2); ?>
+              </div>
+            </div>
             <?php
           }
           ?>
-        </tr>
-          <?php
-          Conexion::abrir_conexion();
-          $re_quote_subitems = ReQuoteSubitemRepository::get_re_quote_subitems_by_id_re_quote_item(Conexion::obtener_conexion(), $re_quote_item-> get_id());
-          Conexion::cerrar_conexion();
-          for($j = 0; $j < count($re_quote_subitems); $j++){
-            $re_quote_subitem = $re_quote_subitems[$j];
-            ?>
-        <tr>
-          <td></td>
-          <td><b>Brand name:</b><?php echo $re_quote_subitem-> get_brand_project(); ?><br><b>Part number:</b><?php echo $re_quote_subitem-> get_part_number_project(); ?><br><b>Item description:</b><br><?php echo nl2br(mb_substr($re_quote_subitem-> get_description_project(), 0, 150)); ?></td>
-          <td><b>Brand name:</b><?php echo $re_quote_subitem-> get_brand(); ?><br><b>Part number:</b> <?php echo $re_quote_item-> get_part_number(); ?><br><b> Item description:</b><br> <?php echo nl2br(mb_substr($re_quote_item-> get_description(), 0, 150)); ?></td>
-          <td style="text-align:right;"><?php echo $re_quote_subitem-> get_quantity(); ?></td>
-            <?php
-              Conexion::abrir_conexion();
-              $re_quote_subitem_providers = ReQuoteSubitemProviderRepository::get_re_quote_subitem_providers_by_id_re_quote_subitem(Conexion::obtener_conexion(), $re_quote_subitem-> get_id());
-              Conexion::cerrar_conexion();
-              if(count($re_quote_subitem_providers)){
-                ?>
-          <td>
-                <?php
-                $prices = [];
-                if(count($re_quote_subitem_providers)){
-                  foreach ($re_quote_subitem_providers as $re_quote_subitem_provider) {
-                    $prices[] = $re_quote_subitem_provider-> get_price();
-                    ?>
-                    <div class="row">
-                      <div class="col-md-6">
-                        <b><?php echo $re_quote_subitem_provider-> get_provider(); ?>:</b>
-                      </div>
-                      <div class="col-md-6">
-                        $ <?php echo $re_quote_subitem_provider-> get_price(); ?>
-                      </div>
-                    </div>
-                    <?php
-                  }
-                }
-                ?>
           </td>
-          <td>$ <?php echo number_format($re_quote_subitem-> get_additional(), 2); ?></td>
+          <td>$ <?php echo number_format($re_quote_item-> get_additional(), 2); ?></td>
           <td>$
-                  <?php
-                  $best_unit_price = min($prices)*$payment_terms*(1+($re_quote-> get_taxes()/100)) + $re_quote_subitem-> get_additional() + $re_quote-> get_additional();
-                  echo number_format($best_unit_price, 2);
-                  ?>
+          <?php
+          $best_unit_price = min($prices)*$payment_terms*(1+($re_quote-> get_taxes()/100)) + $re_quote_item-> get_additional() + $re_quote-> get_additional();
+          echo number_format($best_unit_price, 2);
+          ?>
           </td>
-          <td>$ <?php echo number_format(round($best_unit_price, 2) * $re_quote_subitem-> get_quantity(), 2); ?></td>
-          <td style="text-align:right;">$ <?php echo number_format($re_quote_subitem-> get_unit_price(), 2); ?></td>
-          <td style="text-align:right;">$ <?php echo number_format($re_quote_subitem-> get_total_price(), 2); ?></td>
-                  <?php
-                }else{
+          <td>$ <?php echo number_format(round($best_unit_price, 2) * $re_quote_item-> get_quantity(), 2); ?></td>
+          <td style="text-align:right;">$ <?php echo number_format($re_quote_item-> get_unit_price(), 2); ?></td>
+          <td style="text-align:right;">$ <?php echo number_format($re_quote_item-> get_total_price(), 2); ?></td>
+          <?php
+        }else{
+          ?>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <?php
+        }
+        ?>
+      </tr>
+        <?php
+        Conexion::abrir_conexion();
+        $re_quote_subitems = ReQuoteSubitemRepository::get_re_quote_subitems_by_id_re_quote_item(Conexion::obtener_conexion(), $re_quote_item-> get_id());
+        Conexion::cerrar_conexion();
+        for($j = 0; $j < count($re_quote_subitems); $j++){
+          $re_quote_subitem = $re_quote_subitems[$j];
+          ?>
+      <tr>
+        <td></td>
+        <td><b>Brand name:</b><?php echo $re_quote_subitem-> get_brand_project(); ?><br><b>Part number:</b><?php echo $re_quote_subitem-> get_part_number_project(); ?><br><b>Item description:</b><br><?php echo nl2br(mb_substr($re_quote_subitem-> get_description_project(), 0, 150)); ?></td>
+        <td><b>Brand name:</b><?php echo $re_quote_subitem-> get_brand(); ?><br><b>Part number:</b> <?php echo $re_quote_item-> get_part_number(); ?><br><b> Item description:</b><br> <?php echo nl2br(mb_substr($re_quote_item-> get_description(), 0, 150)); ?></td>
+        <td style="text-align:right;"><?php echo $re_quote_subitem-> get_quantity(); ?></td>
+          <?php
+            Conexion::abrir_conexion();
+            $re_quote_subitem_providers = ReQuoteSubitemProviderRepository::get_re_quote_subitem_providers_by_id_re_quote_subitem(Conexion::obtener_conexion(), $re_quote_subitem-> get_id());
+            Conexion::cerrar_conexion();
+            if(count($re_quote_subitem_providers)){
+              ?>
+        <td>
+              <?php
+              $prices = [];
+              if(count($re_quote_subitem_providers)){
+                foreach ($re_quote_subitem_providers as $re_quote_subitem_provider) {
+                  $prices[] = $re_quote_subitem_provider-> get_price();
                   ?>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
-          <td></td>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <b><?php echo $re_quote_subitem_provider-> get_provider(); ?>:</b>
+                    </div>
+                    <div class="col-md-6">
+                      $ <?php echo $re_quote_subitem_provider-> get_price(); ?>
+                    </div>
+                  </div>
                   <?php
                 }
+              }
+              ?>
+        </td>
+        <td>$ <?php echo number_format($re_quote_subitem-> get_additional(), 2); ?></td>
+        <td>$
+                <?php
+                $best_unit_price = min($prices)*$payment_terms*(1+($re_quote-> get_taxes()/100)) + $re_quote_subitem-> get_additional() + $re_quote-> get_additional();
+                echo number_format($best_unit_price, 2);
                 ?>
-        </tr>
+        </td>
+        <td>$ <?php echo number_format(round($best_unit_price, 2) * $re_quote_subitem-> get_quantity(), 2); ?></td>
+        <td style="text-align:right;">$ <?php echo number_format($re_quote_subitem-> get_unit_price(), 2); ?></td>
+        <td style="text-align:right;">$ <?php echo number_format($re_quote_subitem-> get_total_price(), 2); ?></td>
+                <?php
+              }else{
+                ?>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
                 <?php
               }
-              $a++;
+              ?>
+      </tr>
+              <?php
             }
-      ?>
-        <tr>
-          <td style="border:none;"></td>
-          <td colspan="8" style="font-size:10pt;"><?php echo nl2br($re_quote-> get_shipping()); ?></td>
-          <td style="text-align:right;">$ <?php echo number_format($re_quote-> get_shipping_cost(), 2); ?></td>
-        </tr>
-        <tr>
-          <td style="border:none;"></td>
-          <td style="border:none;"></td>
-          <td style="border:none;"></td>
-          <td style="border:none;"></td>
-          <td style="border:none;"></td>
-          <td style="border:none;"></td>
-          <td style="font-size:12pt;">TOTAL:</td>
-          <td>$ <?php echo number_format($re_quote-> get_total_cost(), 2); ?></td>
-          <td></td>
-          <td style="font-size:12pt;text-align:right;">$ <?php echo number_format($re_quote-> get_total_price(), 2); ?></td>
-        </tr>
-      </table>
-      <?php
-      }
+            $a++;
+          }
+    ?>
+      <tr>
+        <td style="border:none;"></td>
+        <td colspan="8" style="font-size:10pt;"><?php echo nl2br($re_quote-> get_shipping()); ?></td>
+        <td style="text-align:right;">$ <?php echo number_format($re_quote-> get_shipping_cost(), 2); ?></td>
+      </tr>
+      <tr>
+        <td style="border:none;"></td>
+        <td style="border:none;"></td>
+        <td style="border:none;"></td>
+        <td style="border:none;"></td>
+        <td style="border:none;"></td>
+        <td style="border:none;"></td>
+        <td style="font-size:12pt;">TOTAL:</td>
+        <td>$ <?php echo number_format($re_quote-> get_total_cost(), 2); ?></td>
+        <td></td>
+        <td style="font-size:12pt;text-align:right;">$ <?php echo number_format($re_quote-> get_total_price(), 2); ?></td>
+      </tr>
+    </table>
+    <?php
     }
   }
 }
--- plantillas/re_quote.inc.php
+++ plantillas/re_quote.inc.php
@@ -3,37 +3,32 @@ Conexion::abrir_conexion();
 $re_quote_exists = ReQuoteRepository::re_quote_exists(Conexion::obtener_conexion(), $id_rfq);
 $cotizacion = RepositorioRfq::obtener_cotizacion_por_id(Conexion::obtener_conexion(), $id_rfq);
 if(!$re_quote_exists){
-  if($cotizacion-> obtener_canal() == 'FedBid'){
-    $re_quote = new ReQuote('', $id_rfq, $cotizacion-> obtener_total_cost(), $cotizacion-> obtener_total_price(), '', 0, 0, 0, 0, '');
-    $id_re_quote = ReQuoteRepository::insert_re_quote(Conexion::obtener_conexion(), $re_quote);
-  }else{
-    $re_quote = new ReQuote('', $id_rfq, $cotizacion-> obtener_total_cost(), $cotizacion-> obtener_total_price(), $cotizacion-> obtener_payment_terms(), $cotizacion-> obtener_taxes(), $cotizacion-> obtener_profit(), $cotizacion-> obtener_additional(), $cotizacion-> obtener_shipping_cost(), $cotizacion-> obtener_shipping());
-    $id_re_quote = ReQuoteRepository::insert_re_quote(Conexion::obtener_conexion(), $re_quote);
-    $items = RepositorioItem::obtener_items_por_id_rfq(Conexion::obtener_conexion(), $id_rfq);
-    if(count($items)){
-      foreach ($items as $key => $item) {
-        $re_quote_item = new ReQuoteItem('', $id_re_quote, $item-> obtener_brand(), $item-> obtener_brand_project(), $item-> obtener_part_number(), $item-> obtener_part_number_project(), $item-> obtener_description(), $item-> obtener_description_project(), $item-> obtener_quantity(), $item-> obtener_unit_price(), $item-> obtener_total_price(), $item-> obtener_comments(), $item-> obtener_website(), $item-> obtener_additional());
-        $id_re_quote_item = ReQuoteItemRepository::insert_re_quote_item(Conexion::obtener_conexion(), $re_quote_item);
-        $subitems = RepositorioSubitem::obtener_subitems_por_id_item(Conexion::obtener_conexion(), $item-> obtener_id());
-        if(count($subitems)){
-          foreach ($subitems as $key => $subitem) {
-            $re_quote_subitem = new ReQuoteSubitem('', $id_re_quote_item, $subitem-> obtener_brand(), $subitem-> obtener_brand_project(), $subitem-> obtener_part_number(), $subitem-> obtener_part_number_project(), $subitem-> obtener_description(), $subitem-> obtener_description_project(), $subitem-> obtener_quantity(), $subitem-> obtener_unit_price(), $subitem-> obtener_total_price(), $subitem-> obtener_comments(), $subitem-> obtener_website(), $subitem-> obtener_additional());
-            $id_re_quote_subitem = ReQuoteSubitemRepository::insert_re_quote_subitem(Conexion::obtener_conexion(), $re_quote_subitem);
-            $subitem_providers = RepositorioProviderSubitem::obtener_providers_subitem_por_id_subitem(Conexion::obtener_conexion(), $subitem-> obtener_id());
-            if(count($subitem_providers)){
-              foreach ($subitem_providers as $key => $subitem_provider) {
-                $re_quote_subitem_provider = new ReQuoteSubitemProvider('', $id_re_quote_subitem, $subitem_provider-> obtener_provider(), $subitem_provider-> obtener_price());
-                ReQuoteSubitemProviderRepository::insert_re_quote_subitem_provider(Conexion::obtener_conexion(), $re_quote_subitem_provider);
-              }
+  $re_quote = new ReQuote('', $id_rfq, $cotizacion-> obtener_total_cost(), $cotizacion-> obtener_total_price(), $cotizacion-> obtener_payment_terms(), $cotizacion-> obtener_taxes(), $cotizacion-> obtener_profit(), $cotizacion-> obtener_additional(), $cotizacion-> obtener_shipping_cost(), $cotizacion-> obtener_shipping());
+  $id_re_quote = ReQuoteRepository::insert_re_quote(Conexion::obtener_conexion(), $re_quote);
+  $items = RepositorioItem::obtener_items_por_id_rfq(Conexion::obtener_conexion(), $id_rfq);
+  if(count($items)){
+    foreach ($items as $key => $item) {
+      $re_quote_item = new ReQuoteItem('', $id_re_quote, $item-> obtener_brand(), $item-> obtener_brand_project(), $item-> obtener_part_number(), $item-> obtener_part_number_project(), $item-> obtener_description(), $item-> obtener_description_project(), $item-> obtener_quantity(), $item-> obtener_unit_price(), $item-> obtener_total_price(), $item-> obtener_comments(), $item-> obtener_website(), $item-> obtener_additional());
+      $id_re_quote_item = ReQuoteItemRepository::insert_re_quote_item(Conexion::obtener_conexion(), $re_quote_item);
+      $subitems = RepositorioSubitem::obtener_subitems_por_id_item(Conexion::obtener_conexion(), $item-> obtener_id());
+      if(count($subitems)){
+        foreach ($subitems as $key => $subitem) {
+          $re_quote_subitem = new ReQuoteSubitem('', $id_re_quote_item, $subitem-> obtener_brand(), $subitem-> obtener_brand_project(), $subitem-> obtener_part_number(), $subitem-> obtener_part_number_project(), $subitem-> obtener_description(), $subitem-> obtener_description_project(), $subitem-> obtener_quantity(), $subitem-> obtener_unit_price(), $subitem-> obtener_total_price(), $subitem-> obtener_comments(), $subitem-> obtener_website(), $subitem-> obtener_additional());
+          $id_re_quote_subitem = ReQuoteSubitemRepository::insert_re_quote_subitem(Conexion::obtener_conexion(), $re_quote_subitem);
+          $subitem_providers = RepositorioProviderSubitem::obtener_providers_subitem_por_id_subitem(Conexion::obtener_conexion(), $subitem-> obtener_id());
+          if(count($subitem_providers)){
+            foreach ($subitem_providers as $key => $subitem_provider) {
+              $re_quote_subitem_provider = new ReQuoteSubitemProvider('', $id_re_quote_subitem, $subitem_provider-> obtener_provider(), $subitem_provider-> obtener_price());
+              ReQuoteSubitemProviderRepository::insert_re_quote_subitem_provider(Conexion::obtener_conexion(), $re_quote_subitem_provider);
             }
           }
         }
-        $providers = RepositorioProvider::obtener_providers_por_id_item(Conexion::obtener_conexion(), $item-> obtener_id());
-        if(count($providers)){
-          foreach ($providers as $key => $provider) {
-            $re_quote_provider = new ReQuoteProvider('', $id_re_quote_item, $provider-> obtener_provider(), $provider-> obtener_price());
-            ReQuoteProviderRepository::insert_re_quote_provider(Conexion::obtener_conexion(), $re_quote_provider);
-          }
+      }
+      $providers = RepositorioProvider::obtener_providers_por_id_item(Conexion::obtener_conexion(), $item-> obtener_id());
+      if(count($providers)){
+        foreach ($providers as $key => $provider) {
+          $re_quote_provider = new ReQuoteProvider('', $id_re_quote_item, $provider-> obtener_provider(), $provider-> obtener_price());
+          ReQuoteProviderRepository::insert_re_quote_provider(Conexion::obtener_conexion(), $re_quote_provider);
         }
       }
     }
@@ -66,32 +61,13 @@ Conexion::cerrar_conexion();
               </div>
               <div class="card-body">
                 <?php
-                if($cotizacion-> obtener_canal() == 'FedBid'){
-                  ?>
-                  <div class="form-group">
-                    <label for="total_cost">Total cost:</label>
-                    <input type="number" step=".01" class="form-control form-control-sm" name="total_cost" value="<?php echo $re_quote-> get_total_cost(); ?>">
-                  </div>
-                  <div class="form-group">
-                    <label for="total_price">Total price:</label>
-                    <input type="number" step=".01" class="form-control form-control-sm" name="total_price" value="<?php echo $re_quote-> get_total_price(); ?>">
-                  </div>
-                  <?php
-                }else{
                   ReQuoteItemRepository::print_re_quote_items($re_quote-> get_id());
-                }
                 ?>
               </div>
               <div class="card-footer footer_item">
                 <a class="btn btn-primary" id="go_back" href="<?php echo EDITAR_COTIZACION . '/' . $re_quote-> get_id_rfq(); ?>"><i class="fa fa-reply"></i></a>
                 <button type="submit" class="btn btn-success" name="save_re_quote"><i class="fas fa-check"></i> Save</button>
-                <?php
-                if($cotizacion-> obtener_canal() != 'FedBid'){
-                  ?>
-                  <a href="<?php echo ADD_RE_QUOTE_ITEM . $re_quote-> get_id(); ?>" class="btn btn-primary"><i class="fas fa-plus"></i> Add item</a>
-                  <?php
-                }
-                ?>
+                <a href="<?php echo ADD_RE_QUOTE_ITEM . $re_quote-> get_id(); ?>" class="btn btn-primary"><i class="fas fa-plus"></i> Add item</a>
               </div>
             </form>
           </div>
