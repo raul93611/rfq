0a3624726747158ede9aad780738110b5541ff65
raul93611 <raul93611@gmail.com>
2020-04-21 19:39:14 -0400

    trying to improve proposal - failed

--- plantillas/validacion_login.inc.php
+++ plantillas/validacion_login.inc.php
@@ -1,6 +1,5 @@
 <?php
 if(isset($_POST['iniciar_sesion'])){
-  echo 'sdasdsad';
   Conexion::abrir_conexion();
   $validador = new ValidadorLogin($_POST['nombre_usuario'], $_POST['password'], Conexion::obtener_conexion());
   if($validador-> obtener_error() == '' && !is_null($validador-> obtener_usuario())){
