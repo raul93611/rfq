10c8f0a8c624c5cff81344675c7c1f39c2681c77
raul93611 <raul93611@gmail.com>
2020-04-24 19:33:33 -0400

    a fix for an error in console

--- js/tabla.js
+++ js/tabla.js
@@ -176,32 +176,34 @@ $('.fulfillment_table').DataTable({
   'pageLength': 50
 });
 /***************************************************************************************************/
-var time2 = setInterval(function(){
-  var totales = [];
-  $('#re_quote_data tr').each(function(){
-    if (!isNaN($(this).find('td').eq(8).text().split(' ')[1])) {
-      totales.push($(this).find('td').eq(8).text().split(' ')[1]);
-    } else {
-      totales.push(0);
-    }
-  });
-  if(!isNaN($('#shipping_cost_rq').val()) && $('#shipping_cost_rq').val() != ''){
-    var shipping_cost_rq = $('#shipping_cost_rq').val();
-  }else{
-    var shipping_cost_rq = 0;
-  }
-  var total = 0;
-  total = total + parseFloat(shipping_cost_rq);
-  for (var i = 0; i < totales.length; i++) {
-    total = total + parseFloat(totales[i]);
-  }
+  if($('#re_quote_data').length != 0){
+    var time2 = setInterval(function(){
+      var totales = [];
+      $('#re_quote_data tr').each(function(){
+        if (!isNaN($(this).find('td').eq(8).text().split(' ')[1])) {
+          totales.push($(this).find('td').eq(8).text().split(' ')[1]);
+        } else {
+          totales.push(0);
+        }
+      });
+      if(!isNaN($('#shipping_cost_rq').val()) && $('#shipping_cost_rq').val() != ''){
+        var shipping_cost_rq = $('#shipping_cost_rq').val();
+      }else{
+        var shipping_cost_rq = 0;
+      }
+      var total = 0;
+      total = total + parseFloat(shipping_cost_rq);
+      for (var i = 0; i < totales.length; i++) {
+        total = total + parseFloat(totales[i]);
+      }
 
-  $('#total_re_quote').html('$ ' + total.toFixed(2));
-  $('#total_cost').val(total);
-  var profit_rq = (parseFloat($('#total_ganado').html().split(' ')[1]) - total).toFixed(2);
-  var percentage_profit_rq = ((profit_rq/total)*100).toFixed(2);
-  $('#profit_rq').html('$ ' + profit_rq + '<br>' + percentage_profit_rq + '%');
-}, 500);
+      $('#total_re_quote').html('$ ' + total.toFixed(2));
+      $('#total_cost').val(total);
+      var profit_rq = (parseFloat($('#total_ganado').html().split(' ')[1]) - total).toFixed(2);
+      var percentage_profit_rq = ((profit_rq/total)*100).toFixed(2);
+      $('#profit_rq').html('$ ' + profit_rq + '<br>' + percentage_profit_rq + '%');
+    }, 500);
+  }
 
 $('#re_quote_form').submit(function () {
   var totales = [];
