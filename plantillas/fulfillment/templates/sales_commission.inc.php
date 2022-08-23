f57430192d0c1b82d67a21d07fb183d4f6e47d68
Raul Velasco <raul93611@gmail.com>
2022-08-22 20:15:43 -0400

    Sales commission option selected is displayed

--- plantillas/fulfillment/templates/sales_commission.inc.php
+++ plantillas/fulfillment/templates/sales_commission.inc.php
@@ -4,29 +4,31 @@ $re_quote = ReQuoteRepository::get_re_quote_by_id_rfq(Conexion::obtener_conexion
 Conexion::cerrar_conexion();
 switch ($quote->obtener_sales_commission()) {
   case 'No commission':
-    $sales_commission = ['$0', '$0'];
+    $sales_commission = ['$0', '$0', 'No commission'];
     break;
   case 'Same commission':
     $sales_commission = [
       '$' . number_format($quote->obtener_quote_total_price() - $re_quote->get_total_cost(), 2) . ' / ' . number_format(100 * (($quote->obtener_quote_total_price() - $re_quote->get_total_cost()) / $quote->obtener_quote_total_price()), 2) . '%',
-      '$' . number_format(($quote->obtener_quote_total_price() - $re_quote->get_total_cost()) * 0.03, 2)
+      '$' . number_format(($quote->obtener_quote_total_price() - $re_quote->get_total_cost()) * 0.03, 2),
+      'Same commission'
     ];
     break;
   case 'Other commission':
     $sales_commission = [
       '$' . number_format($quote->obtener_real_fulfillment_profit(), 2) . ' / ' . number_format($quote->obtener_real_fulfillment_profit_percentage(), 2) . '%',
-      '$' . number_format($quote->obtener_real_fulfillment_profit() * 0.03, 2)
+      '$' . number_format($quote->obtener_real_fulfillment_profit() * 0.03, 2),
+      'Other commission'
     ];
     break;
   default:
-    $sales_commission = ['Not set', 'Not set'];
+    $sales_commission = ['Not set', 'Not set', 'Not set'];
     break;
 }
 ?>
 <div class="container-fluid sales-commission">
   <div class="row">
     <div class="col-md-6">
-      <h5 class="text-success"><?php echo 'Profit basis for commission: ' . $sales_commission[0]; ?>
+      <h5 class="text-success"><?php echo 'Profit basis for commission (' . $sales_commission[2] . '): ' . $sales_commission[0]; ?>
         <button type="button" class="btn btn-link text-success" data-toggle="tooltip" data-html="true" title="<?php echo $quote-> obtener_sales_commission_comment(); ?>">
           <i class="fas fa-comment"></i>
         </button>
