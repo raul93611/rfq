d5394b27e8f0d8c774389659cfa1a0c4cd6ec3c9
Raul Velasco <raul93611@gmail.com>
2023-03-23 13:47:49 -0400

    fix in delete quote script

--- scripts/quote/delete_quote.php
+++ scripts/quote/delete_quote.php
@@ -1,6 +1,7 @@
 <?php
 Conexion::abrir_conexion();
-RepositorioRfq::delete_quote(Conexion::obtener_conexion(), $cotizacion-> obtener_id());
+$quote = RepositorioRfq::obtener_cotizacion_por_id(Conexion::obtener_conexion(), $id_rfq);
+RepositorioRfq::delete_quote(Conexion::obtener_conexion(), $quote-> obtener_id());
 Conexion::cerrar_conexion();
-Redireccion::redirigir(CHANNEL . $cotizacion-> obtener_canal());
+Redireccion::redirigir(CHANNEL . $quote-> obtener_canal());
 ?>
