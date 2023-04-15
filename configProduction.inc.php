164ae030c933e60077938ce2af5df44c6fe40499
Raul Velasco <raul93611@gmail.com>
2023-04-15 09:55:18 -0400

    gitignore update

--- configProduction.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('NOMBRE_SERVIDOR', 'elogicportalcom.globatmysql.com');
-define('NOMBRE_USUARIO', 'elogicdb');
-define('PASSWORD', 'elogicdb');
-define('NOMBRE_BD', 'elogicnewdb');
-
-define('SERVIDOR', 'https://www.elogicportal.com/rfq/');
-define('ERROR', SERVIDOR . 'error');
-?>
--- email.php
+++
@@ -1,3 +0,0 @@
-<?php
-Email::send_email_task_created('raul93611@gmail.com', '');
-?>
