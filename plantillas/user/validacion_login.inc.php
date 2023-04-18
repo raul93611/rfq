fbcf1d86b6ce1166c8d3865d359ee5483f95c03c
Raul Velasco <raul93611@gmail.com>
2023-04-18 11:33:54 -0400

    temporary fix about charts

--- plantillas/user/validacion_login.inc.php
+++ plantillas/user/validacion_login.inc.php
@@ -5,11 +5,12 @@ if(isset($_POST['iniciar_sesion'])){
   Conexion::cerrar_conexion();
   if($validador-> obtener_error() == '' && !is_null($validador-> obtener_usuario())){
     ControlSesion::iniciar_sesion($validador-> obtener_usuario());
-    if($_SESSION['user']-> is_admin() || $_SESSION['user']-> is_fulfillment() || $_SESSION['user']-> is_accounting()){
-      Redireccion::redirigir1(ALL_TASKS);
-    }else if($_SESSION['user']-> is_rfq()){
-      Redireccion::redirigir1(CHARTS);
-    }
+    // if($_SESSION['user']-> is_admin() || $_SESSION['user']-> is_fulfillment() || $_SESSION['user']-> is_accounting()){
+    //   Redireccion::redirigir1(ALL_TASKS);
+    // }else if($_SESSION['user']-> is_rfq()){
+    //   Redireccion::redirigir1(CHARTS);
+    // }
+    Redireccion::redirigir1(ALL_TASKS);
   }
 }
 ?>
