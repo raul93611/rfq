06bb53e467bb1b176894a4f1355379c00843cbcb
Raul Velasco <raul93611@gmail.com>
2023-05-17 06:59:10 -0400

    fix requote only services 2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+// error_reporting(E_ALL);
+// ini_set('display_errors', '1');
 include_once 'app/Bootstrap/config.inc.php';
 include_once 'app/Bootstrap/routes.inc.php';
 include_once 'app/Bootstrap/Conexion.inc.php';
