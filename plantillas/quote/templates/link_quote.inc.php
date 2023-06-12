1a8513749042603ab99dec17f660ace93e7a11f3
Raul Velasco <raul93611@gmail.com>
2023-06-12 19:19:30 -0400

    fix to display the link button

--- plantillas/quote/templates/link_quote.inc.php
+++ plantillas/quote/templates/link_quote.inc.php
@@ -1,4 +1,4 @@
-<?php if(is_null($cotizacion_recuperada-> obtener_multi_year_project())): ?>
+<?php if(!$cotizacion_recuperada-> obtener_multi_year_project()): ?>
 <small>
   <a href="#" id="link_quote_button">
     <i class="h6 fas fa-link"></i>
