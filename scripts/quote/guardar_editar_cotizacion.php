a154129374219ec18a9a4abe3b828a8b58d55c94
Raul Velasco <raul93611@gmail.com>
2023-09-04 09:03:42 -0400

    save quote improvements

--- scripts/quote/guardar_editar_cotizacion.php
+++ scripts/quote/guardar_editar_cotizacion.php
@@ -2,13 +2,49 @@
 if (isset($_POST['guardar_cambios_cotizacion'])) {
   Conexion::abrir_conexion();
   $cotizacion_recuperada = RepositorioRfq::obtener_cotizacion_por_id(Conexion::obtener_conexion(), $_POST['id_rfq']);
-  RepositorioRfq::insert_calc(Conexion::obtener_conexion(), $_POST['id_items'], $_POST['id_subitems'], $_POST['partes_total_price'], $_POST['partes_total_price_subitems'], $_POST['unit_prices'], $_POST['unit_prices_subitems'], $_POST['additional'], $_POST['additional_subitems']);
-  RepositorioRfq::update_variables(Conexion::obtener_conexion(), $_POST['payment_terms'], $_POST['taxes'], $_POST['profit'], $_POST['total_cost'], $_POST['total_price'], $_POST['additional_general'], htmlspecialchars($_POST['shipping']), $_POST['shipping_cost'], $_POST['id_rfq']);
+  RepositorioRfq::insert_calc(
+    Conexion::obtener_conexion(), 
+    $_POST['id_items'] ?? '', 
+    $_POST['id_subitems'] ?? '', 
+    $_POST['partes_total_price'] ?? '', 
+    $_POST['partes_total_price_subitems'] ?? '', 
+    $_POST['unit_prices'] ?? '', 
+    $_POST['unit_prices_subitems'] ?? '', 
+    $_POST['additional'] ?? '', 
+    $_POST['additional_subitems'] ?? ''
+  );
+  RepositorioRfq::update_variables(
+    Conexion::obtener_conexion(), 
+    $_POST['payment_terms'] ?? '', 
+    $_POST['taxes'] ?? 0, 
+    $_POST['profit'] ?? 0,  
+    $_POST['total_cost'] ?? 0, 
+    $_POST['total_price'] ?? 0, 
+    $_POST['additional_general'] ?? '', 
+    htmlspecialchars($_POST['shipping'] ?? ''),  
+    $_POST['shipping_cost'] ?? 0, 
+    $_POST['id_rfq']
+  );
   if($cotizacion_recuperada-> obtener_canal() == 'Chemonics'){
     RepositorioRfq::guardar_total_price_chemonics(Conexion::obtener_conexion(), $_POST['total_price_chemonics'], $_POST['id_rfq']);
   }
   $cotizacion_recuperada = RepositorioRfq::obtener_cotizacion_por_id(Conexion::obtener_conexion(), $_POST['id_rfq']);
-  AuditTrailRepository::items_table_events(Conexion::obtener_conexion(), $_POST['taxes'], $_POST['taxes_original'], $_POST['profit'], $_POST['profit_original'], $_POST['additional_general'], $_POST['additional_general_original'], $_POST['payment_terms'], $_POST['payment_terms_original'], $_POST['shipping'], $_POST['shipping_original'], $_POST['shipping_cost'], $_POST['shipping_cost_original'], $_POST['id_rfq']);
+  AuditTrailRepository::items_table_events(
+    Conexion::obtener_conexion(), 
+    $_POST['taxes'] ?? '', 
+    $_POST['taxes_original'] ?? '', 
+    $_POST['profit'] ?? '', 
+    $_POST['profit_original'] ?? '', 
+    $_POST['additional_general'] ?? '', 
+    $_POST['additional_general_original'] ?? '', 
+    $_POST['payment_terms'] ?? '', 
+    $_POST['payment_terms_original'] ?? '', 
+    $_POST['shipping'] ?? '', 
+    $_POST['shipping_original'] ?? '', 
+    $_POST['shipping_cost'] ?? '', 
+    $_POST['shipping_cost_original'] ?? '', 
+    $_POST['id_rfq']
+  );
   ServiceRepository::calc_items_with_CC(Conexion::obtener_conexion(), $_POST['services_payment_term'] ?? null, $_POST['id_rfq']);
   Conexion::cerrar_conexion();
   if($cotizacion_recuperada-> obtener_canal() == 'Chemonics'){
@@ -81,4 +117,3 @@ if (isset($_POST['guardar_cambios_cotizacion'])) {
   }
   Redireccion::redirigir(EDITAR_COTIZACION . '/' . $_POST['id_rfq']);
 }
-?>
