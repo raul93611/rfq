362c79e0a11eb8f4cc84ef4d2e1e46654f556e9e
Raul Velasco <raul93611@gmail.com>
2024-03-05 15:06:41 -0400

    delete and restore quote audit trails

--- scripts/quote/delete_quote.php
+++ scripts/quote/delete_quote.php
@@ -2,6 +2,7 @@
 Conexion::abrir_conexion();
 $quote = RepositorioRfq::obtener_cotizacion_por_id(Conexion::obtener_conexion(), $id_rfq);
 RepositorioRfq::delete_quote(Conexion::obtener_conexion(), $quote->obtener_id());
+AuditTrailRepository::quote_status_audit_trail(Conexion::obtener_conexion(), 'Deleted', $id_rfq);
 Conexion::cerrar_conexion();
 if ($quote->isNobid()) {
   Redireccion::redirigir(NO_BID);
--- scripts/quote/restore_quote.php
+++ scripts/quote/restore_quote.php
@@ -1,6 +1,7 @@
 <?php
 Conexion::abrir_conexion();
 RepositorioRfq::restore_quote(Conexion::obtener_conexion(), $id_rfq);
+AuditTrailRepository::quote_status_audit_trail(Conexion::obtener_conexion(), 'Restored', $id_rfq);
 Conexion::cerrar_conexion();
 
 Redireccion::redirigir(DELETED);
