9d4d2b5998fafabd92af6417c57d20b7c9c1a5a7
Raul Velasco <raul93611@gmail.com>
2024-03-27 14:49:27 -0400

    small fix in the file upload

--- app/Utilities/Input.inc.php
+++ app/Utilities/Input.inc.php
@@ -1,17 +1,17 @@
 <?php
-class Input{
-  public static function print_designated_user($quote){
-    if ($quote->obtener_completado() || $quote-> obtener_status()) {
+class Input {
+  public static function print_designated_user($quote) {
+    if ($quote->obtener_completado() || $quote->obtener_status()) {
       Conexion::abrir_conexion();
       $usuario = self::get_designated_user($quote);
       Conexion::cerrar_conexion();
-      ?>
+?>
       <label for="usuario_designado">Designated user:</label>
       <input type="text" name="usuario_designado" class="form-control form-control-sm" value="<?php echo $usuario; ?>" readonly>
       <input type="hidden" name="designated_user_original" value="<?php echo $usuario; ?>">
-      <?php
+    <?php
     } else {
-      ?>
+    ?>
       <div class="form-group">
         <?php
         Conexion::abrir_conexion();
@@ -21,44 +21,44 @@ class Input{
         ?>
         <?php
         if (count($usuarios)) {
-          ?>
+        ?>
           <label for="usuario_designado">Designated user:</label>
           <input type="hidden" name="designated_user_original" value="<?php echo $designated_user; ?>">
           <select id="usuario_designado" class="form-control form-control-sm" name="usuario_designado">
             <?php
             foreach ($usuarios as $usuario) {
-              ?>
+            ?>
               <option <?php
-              if ($usuario->obtener_id() == $quote->obtener_usuario_designado()) {
-                echo 'selected';
-              }
-              ?>><?php echo $usuario->obtener_nombre_usuario(); ?></option>
-                <?php
-              }
-              ?>
+                      if ($usuario->obtener_id() == $quote->obtener_usuario_designado()) {
+                        echo 'selected';
+                      }
+                      ?>><?php echo $usuario->obtener_nombre_usuario(); ?></option>
+            <?php
+            }
+            ?>
           </select>
-          <?php
+        <?php
         }
         ?>
       </div>
-      <?php
+    <?php
     }
   }
 
-  public static function get_designated_user($quote){
+  public static function get_designated_user($quote) {
     Conexion::abrir_conexion();
     $usuarios = RepositorioUsuario::getAllRfqUsers(Conexion::obtener_conexion());
     Conexion::cerrar_conexion();
     foreach ($usuarios as $usuario) {
       if ($usuario->obtener_id() == $quote->obtener_usuario_designado()) {
-        $designated_user = $usuario-> obtener_nombre_usuario();
+        $designated_user = $usuario->obtener_nombre_usuario();
       }
     }
 
     return $designated_user;
   }
 
-  public static function printable_channel($channel){
+  public static function printable_channel($channel) {
     $canal = $channel;
     switch ($channel) {
       case 'FedBid':
@@ -71,19 +71,21 @@ class Input{
     return $canal;
   }
 
-  public static function save_files($path, $files, $id_rfq){
-    mkdir($path, 0777);
+  public static function save_files($path, $files, $id_rfq) {
+    if (!is_dir($path)) {
+      mkdir($path, 0777, true); 
+    }
     $documentos = array_filter($files['name']);
     $total = count($documentos);
     for ($i = 0; $i < $total; $i++) {
       $tmp_path = $files['tmp_name'][$i];
       $file = $files['name'][$i];
       if ($tmp_path != '') {
-        $file = preg_replace('/[^a-z0-9-_\-\.]/i','_',$file);
+        $file = preg_replace('/[^a-z0-9-_\-\.]/i', '_', $file);
         $new_path = $path . '/' . $file;
         move_uploaded_file($tmp_path, $new_path);
       }
-      if(!empty($file)){
+      if (!empty($file)) {
         Conexion::abrir_conexion();
         AuditTrailRepository::document_updated(Conexion::obtener_conexion(), 'uploaded', $file, $id_rfq);
         Conexion::cerrar_conexion();
@@ -91,13 +93,13 @@ class Input{
     }
   }
 
-  public static function copy_files($initial_path, $destination_path){
+  public static function copy_files($initial_path, $destination_path) {
     mkdir($destination_path, 0777);
-    if(is_dir($initial_path)){
+    if (is_dir($initial_path)) {
       $manager = opendir($initial_path);
       $folder = @scandir($initial_path);
-      while(($file = readdir($manager)) !== false){
-        if($file != '.' && $file != '..'){
+      while (($file = readdir($manager)) !== false) {
+        if ($file != '.' && $file != '..') {
           copy($initial_path . '/' . $file, $destination_path . '/' . $file);
         }
       }
@@ -105,18 +107,18 @@ class Input{
     }
   }
 
-  public static function print_year_select($year){
+  public static function print_year_select($year) {
     ?>
     <select name="year" class="custom-select">
-    <?php
-    for ($i=2018; $i<=2100; $i++) {
-      ?>
-      <option <?php echo $year == $i ? 'selected' : ''; ?>><?php echo $i; ?></option>
       <?php
-    }
-    ?>
+      for ($i = 2018; $i <= 2100; $i++) {
+      ?>
+        <option <?php echo $year == $i ? 'selected' : ''; ?>><?php echo $i; ?></option>
+      <?php
+      }
+      ?>
     </select>
-    <?php
+<?php
   }
 
   public static function test_input($data) {
