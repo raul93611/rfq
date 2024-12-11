e6cad5dd6769a930952c46aaf6244aea3cd8a595
Raul Velasco <raul93611@gmail.com>
2024-12-11 17:17:57 -0400

    tracking fixes

--- herramientas/pdfTemplates/tracking.inc.php
+++ herramientas/pdfTemplates/tracking.inc.php
@@ -4,7 +4,7 @@
 <head>
   <style>
     body {
-      font-family: Roboto, sans-serif;
+      font-family: roboto;
     }
 
     th {
@@ -15,7 +15,11 @@
     .tabla th,
     .tabla td {
       border: 1px solid #DEE8F2;
-      padding: 5px 10px;
+
+      padding-left: 10px;
+      padding-right: 10px;
+      padding-top: 5px;
+      padding-bottom: 5px;
       font-size: 7pt;
     }
 
@@ -33,20 +37,26 @@
       width: 20px;
     }
 
-    .letra_grande {
-      font-size: 15pt;
+    .total_ancho {
+      width: 130px;
+    }
+
+    .letra_chiquita {
+      font-size: 8pt;
     }
 
     .color {
       color: #004A97;
+    }
+
+    .letra_grande {
+      font-size: 15pt;
     }
   </style>
 </head>
 
 <body>
   <h2 class="color" style="text-align:center;">TRACKING DETAILS</h2>
-
-  <!-- Proposal and Contract Table -->
   <table class="tabla">
     <tr>
       <th style="font-size:9pt;">PROPOSAL #</th>
@@ -54,18 +64,16 @@
     </tr>
     <tr>
       <td style="text-align:center;font-size:9pt;"><?= $cotizacion->obtener_id() ?></td>
-      <td style="text-align:center;font-size:9pt;"><?= $cotizacion->obtener_contract_number() ?></td>
+      <td style="text-align:center;font-size:9pt;"><?= $cotizacion->obtener_contract_number() ?></ts>
     </tr>
   </table>
-
-  <?php if (!empty($items)) : ?>
-    <!-- Items Table -->
-    <table class="tabla" style="width:100%; margin-top: 20px;">
+  <?php if (count($items)) : ?>
+    <table class="tabla" style="width:100%;">
       <tr>
         <th class="quantity">#</th>
-        <th>PROJECT SPEC.</th>
-        <th class="quantity">QTY (Ordered)</th>
-        <th class="quantity">QTY (Shipped)</th>
+        <th>PROJECT ESPC.</th>
+        <th class="quantity">QTY(ordered)</th>
+        <th class="quantity">QTY(shipped)</th>
         <th>CARRIER</th>
         <th>TRACKING #</th>
         <th>DELIVERY DATE</th>
@@ -73,95 +81,116 @@
         <th>SIGNED BY</th>
         <th>COMMENT</th>
       </tr>
-
       <?php
       $a = 1;
-      Conexion::abrir_conexion();
-      foreach ($items as $i => $item) :
+      for ($i = 0; $i < count($items); $i++) :
+        $item = $items[$i];
         $re_quote_item = $re_quote_items[$i];
+        Conexion::abrir_conexion();
         $trackings = TrackingRepository::get_all_trackings_by_id_item(Conexion::obtener_conexion(), $item->obtener_id());
+        Conexion::cerrar_conexion();
+      ?>
+        <tr>
+          <td style="border-bottom: 0px solid;"><?= $a ?></td>
+          <td style="border-bottom: 0px solid;">
+            <b>Brand name:</b> <?= $re_quote_item->get_brand() ?>
+            <br>
+            <b>Part number:</b> <?= $re_quote_item->get_part_number() ?>
+            <br>
+            <b>Item description:</b> <?= nl2br(wordwrap(mb_substr($re_quote_item->get_description(), 0, 150), 70, '<br>', true)) ?>
+          </td>
+          <td style="text-align:right;border-bottom: 0px solid;"><?= $re_quote_item->get_quantity() ?></td>
+          <?php if (count($trackings)) : ?>
+            <td><?= $trackings[0]->get_quantity() ?></td>
+            <td><?= $trackings[0]->get_carrier() ?></td>
+            <td><?= nl2br($trackings[0]->get_tracking_number()) ?></td>
+            <td><?= RepositorioComment::mysql_date_to_english_format($trackings[0]->get_delivery_date()) ?></td>
+            <td><?= RepositorioComment::mysql_date_to_english_format($trackings[0]->get_due_date()) ?></td>
+            <td><?= $trackings[0]->get_signed_by() ?></td>
+            <td><?= nl2br($trackings[0]->get_comments()) ?></td>
+        </tr>
+        <?php
+            for ($j = 1; $j < count($trackings); $j++) :
+              $tracking = $trackings[$j];
+        ?>
+          <tr>
+            <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+            <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+            <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+            <td><?= $tracking->get_quantity() ?></td>
+            <td><?= $tracking->get_carrier() ?></td>
+            <td><?= nl2br($tracking->get_tracking_number()) ?></td>
+            <td><?= RepositorioComment::mysql_date_to_english_format($tracking->get_delivery_date()) ?></td>
+            <td><?= RepositorioComment::mysql_date_to_english_format($tracking->get_due_date()) ?></td>
+            <td><?= $tracking->get_signed_by() ?></td>
+            <td><?= nl2br($tracking->get_comments()) ?></td>
+          </tr>
+        <?php endfor; ?>
+      <?php endif; ?>
+      <?php
+        Conexion::abrir_conexion();
         $subitems = RepositorioSubitem::obtener_subitems_por_id_item(Conexion::obtener_conexion(), $item->obtener_id());
         $re_quote_subitems = ReQuoteSubitemRepository::get_re_quote_subitems_by_id_re_quote_item(Conexion::obtener_conexion(), $re_quote_item->get_id());
+        Conexion::cerrar_conexion();
+        if (count($subitems)) :
+          for ($k = 0; $k < count($subitems); $k++) :
+            $subitem = $subitems[$k];
+            $re_quote_subitem = $re_quote_subitems[$k];
+            Conexion::abrir_conexion();
+            $trackings_subitems = TrackingSubitemRepository::get_all_trackings_by_id_subitem(Conexion::obtener_conexion(), $subitem->obtener_id());
+            Conexion::cerrar_conexion();
       ?>
-
-        <!-- Item Row -->
-        <?= renderTrackingRow($a++, $re_quote_item, $trackings) ?>
-
-        <!-- Subitem Rows -->
-        <?php foreach ($subitems as $k => $subitem) :
-          $re_quote_subitem = $re_quote_subitems[$k];
-          $trackings_subitems = TrackingSubitemRepository::get_all_trackings_by_id_subitem(Conexion::obtener_conexion(), $subitem->obtener_id());
-        ?>
-          <?= renderTrackingRow(null, $re_quote_subitem, $trackings_subitems, true) ?>
-        <?php endforeach; ?>
-
-      <?php endforeach;
-      Conexion::cerrar_conexion();
-      ?>
-      <!-- Footer Note -->
-      <tr>
-        <td colspan="10" style="text-align:center;">
-          <b>E-Logic</b> wants you to be satisfied with your purchase. Before accepting delivery, please inspect the items. If there are issues, you may refuse delivery. After acceptance, you have 30 days to contact us regarding defects or damage. Replacement requests cannot be processed after this period.
-        </td>
-      </tr>
+          <tr>
+            <td style="border-bottom: 0px solid;"></td>
+            <td style="border-bottom: 0px solid;">
+              <b>Brand name:</b> <?= $re_quote_subitem->get_brand() ?>
+              <br>
+              <b>Part number:</b> <?= $re_quote_subitem->get_part_number() ?>
+              <br>
+              <b>Item description:</b>
+              <br> <?= nl2br(wordwrap(mb_substr($re_quote_subitem->get_description(), 0, 150), 70, '<br>', true)) ?>
+            </td>
+            <td style="text-align:right;border-bottom: 0px solid;"><?= $re_quote_subitem->get_quantity() ?></td>
+            <?php if (count($trackings_subitems)) : ?>
+              <td><?= $trackings_subitems[0]->get_quantity() ?></td>
+              <td><?= $trackings_subitems[0]->get_carrier() ?></td>
+              <td><?= nl2br($trackings_subitems[0]->get_tracking_number()) ?></td>
+              <td><?= RepositorioComment::mysql_date_to_english_format($trackings_subitems[0]->get_delivery_date()) ?></td>
+              <td><?= RepositorioComment::mysql_date_to_english_format($trackings_subitems[0]->get_due_date()) ?></td>
+              <td><?= $trackings_subitems[0]->get_signed_by() ?></td>
+              <td><?= nl2br($trackings_subitems[0]->get_comments()) ?></td>
+          </tr>
+          <?php
+              for ($l = 1; $l < count($trackings_subitems); $l++) :
+                $tracking_subitem = $trackings_subitems[$l];
+          ?>
+            <tr>
+              <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+              <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+              <td style="border-top: 0px solid;border-bottom: 0px solid;"></td>
+              <td><?= $tracking_subitem->get_quantity() ?></td>
+              <td><?= $tracking_subitem->get_carrier() ?></td>
+              <td><?= nl2br($tracking_subitem->get_tracking_number()) ?></td>
+              <td><?= RepositorioComment::mysql_date_to_english_format($tracking_subitem->get_delivery_date()) ?></td>
+              <td><?= RepositorioComment::mysql_date_to_english_format($tracking_subitem->get_due_date()) ?></td>
+              <td><?= $tracking_subitem->get_signed_by() ?></td>
+              <td><?= nl2br($tracking_subitem->get_comments()) ?></td>
+            </tr>
+          <?php endfor; ?>
+        <?php endif; ?>
+      <?php endfor; ?>
+    <?php
+        endif;
+        $a++;
+    ?>
+  <?php endfor; ?>
+  <tr>
+    <td colspan="10" style="text-align:center;">
+      <b>E-Logic</b> wants you to be satisfied with your purchase, help us to achieve it. Before you accept the delivery of your items, please inspect it. If any issue exists, you may refuse delivery. Once you have accepted delivery you have 30 days to contact us regarding defects, damage or other issues. <b>E-Logic</b> will not be able to arrange a replacement once this period has elapsed.
+    </td>
+  </tr>
     </table>
   <?php endif; ?>
 </body>
 
 </html>
-
-<?php
-/**
- * Renders tracking rows for items and subitems.
- *
- * @param int|null $index Index number or null for subitems.
- * @param object $quoteItem The quote item or subitem object.
- * @param array $trackings The list of trackings for the item or subitem.
- * @param bool $isSubitem Whether this is a subitem row.
- * @return string Rendered HTML rows.
- */
-function renderTrackingRow($index, $quoteItem, $trackings, $isSubitem = false) {
-  ob_start();
-?>
-  <tr>
-    <td style="text-align:center;"><?= $index ?? '' ?></td>
-    <td>
-      <b>Brand name:</b> <?= $quoteItem->get_brand() ?><br>
-      <b>Part number:</b> <?= $quoteItem->get_part_number() ?><br>
-      <b>Item description:</b> <?= nl2br(wordwrap(mb_substr($quoteItem->get_description(), 0, 150), 70, '<br>', true)) ?>
-    </td>
-    <td style="text-align:right;"><?= $quoteItem->get_quantity() ?></td>
-    <?php if (!empty($trackings)) : ?>
-      <?= renderTrackingDetails($trackings[0]) ?>
-  </tr>
-  <?php for ($j = 1; $j < count($trackings); $j++) : ?>
-    <tr>
-      <td colspan="<?= $isSubitem ? 3 : 2 ?>"></td>
-      <?= renderTrackingDetails($trackings[$j]) ?>
-    </tr>
-  <?php endfor; ?>
-<?php else : ?>
-  <td colspan="7" style="text-align:center;">No tracking details available.</td>
-<?php endif; ?>
-<?php
-  return ob_get_clean();
-}
-
-/**
- * Renders tracking details for a single tracking entry.
- *
- * @param object $tracking Tracking object.
- * @return string Rendered HTML cells.
- */
-function renderTrackingDetails($tracking) {
-  return sprintf(
-    '<td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td>',
-    $tracking->get_quantity(),
-    $tracking->get_carrier(),
-    nl2br($tracking->get_tracking_number()),
-    RepositorioComment::mysql_date_to_english_format($tracking->get_delivery_date()),
-    RepositorioComment::mysql_date_to_english_format($tracking->get_due_date()),
-    nl2br($tracking->get_comments())
-  );
-}
-?>
